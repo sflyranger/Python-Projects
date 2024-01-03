dc76f7a484971b687f951455bd5821731656a96d
Steven Evans <151431504+sflyranger@users.noreply.github.com>
2024-01-02 19:00:01 -0800

    Empiree State Algorithm

---
+++ First_Use_Project/Empire State Project.py
@@ -0,0 +1,126 @@
+# Import numpy as np
+import numpy as np
+
+# Set the seed, this causes the data to be replicated and results to be reusable when the seed is set before running.
+np.random.seed(123)
+
+# Generate and print random float
+print(np.random.rand())
+
+# Printing out random dice rolls with randint
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Use randint() to simulate a dice
+print(np.random.randint(1,7))
+
+# Use randint() again
+print(np.random.randint(1,7))
+
+# Creating if else elif statment to step up or down or re roll dice.
+# Import Numpy and set seed.
+import numpy as np
+np.random.seed(123)
+# Starting step
+step = 50
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <= 5:
+    step = step + 1
+else:
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice)
+print(step)
+
+
+# Import numpy and set seed
+import numpy as np
+np.random.seed(123)
+
+# Initialize random_walk
+random_walk = [0]
+
+# Complete the ___
+for x in range(100) :
+    # Set step: last element in random_walk
+    step = random_walk[-1]
+
+    # Roll the dice
+    dice = np.random.randint(1,7)
+
+    # Determine next step
+    if dice <= 2:
+        step = step - 1
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    # append next_step to random_walk
+    random_walk.append(step)
+
+# Print random_walk
+print(random_walk)
+print(step)
+
+
+# NumPy is imported, seed is set
+import numpy as np
+np.random.seed(123)
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step = max(0,step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
+print(step)
+
+
+# NumPy is imported, seed is set
+import numpy as np
+np.random.seed(123)
+
+# Initialization
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+# Importing matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
