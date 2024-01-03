f7d266cc911840d8886cd03064062ea786d1f5fa
Steven Evans <151431504+sflyranger@users.noreply.github.com>
2024-01-02 19:42:52 -0800

    Empiree State changes

--- First_Use_Project/Empire State Project.py
+++ First_Use_Project/Empire State Project.py
@@ -124,3 +124,140 @@ plt.plot(random_walk)
 
 # Show the plot
 plt.show()
+
+# Creating a list of random walks to further plot on a  line plot and histogram for distribution.
+# NumPy is imported; seed is set
+import numpy as np
+np.random.seed(123)
+# Initialize all_walks (don't change this line)
+all_walks = []
+
+# Simulate random walk five times
+for i in range(5) :
+
+    # Code from before
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+
+    # Append random_walk to all_walks
+    all_walks.append(random_walk)
+
+# Print all_walks
+print(all_walks)
+
+
+# numpy and matplotlib imported, seed set.
+import numpy as np
+import matplotlib.pyplot as plt
+# initialize and populate all_walks
+all_walks = []
+for i in range(5) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Convert all_walks to NumPy array: np_aw
+np_aw = np.array(all_walks)
+
+# Plot np_aw and show
+plt.plot(np_aw)
+plt.show()
+
+# Clear the figure
+plt.clf()
+
+# Transpose np_aw: np_aw_t
+np_aw_t= np.transpose(all_walks)
+
+# Plot np_aw_t and show
+plt.plot(np_aw_t)
+plt.show()
+
+
+# numpy and matplotlib imported, seed set same as above
+
+# clear the plot so it doesn't get cluttered if you run this many times
+plt.clf()
+
+# Simulate random walk 20 times
+all_walks = []
+for i in range(20) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness as us having a 0.5% chance of falling all the way to the bottom of the tower.
+        # This generates a random number for each iteration, if the number is less than or equal to 0.005 then we fall.
+        if np.random.rand() <= 0.005 :
+            step = 0
+
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+plt.plot(np_aw_t)
+plt.show()
+
+
+
+# numpy and matplotlib imported, seed set
+
+# Simulate random walk 500 times
+all_walks = []
+for i in range(500) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+        if np.random.rand() <= 0.001 :
+            step = 0
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+
+# Select last row from np_aw_t: ends, selects all
+ends = np_aw_t[-1,:]
+
+# Plot histogram of ends, display plot
+plt.hist(ends)
+plt.show()
+
+
+# Percentage of results from the array that are greater than or equal to 60
+np.mean(ends >=60)
+0.784
